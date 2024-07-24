c9ac8fe7d04b1372971f3d9f2079b555b83ba241
ayazibrahimovv <ayazibrahimov.official@gmail.com>
2024-07-24 23:47:37 +0400

    sasa

--- index-1.py
+++ index-1.py
@@ -1,2 +1,51 @@
-print('Hello World')
-print('Ayaz')
+# print('Hello World')
+
+# print(3)
+
+# print(type(3.5))
+
+
+# name = 'Ayaz'
+# name = 'Asef'
+
+# print(type(name))
+# print(name)
+
+# a = 5
+# b = 10
+
+# a,b = b,a
+
+# print(a)
+# print(b)
+
+# i = 10
+
+# i+=1
+
+# print(i)
+
+# a = -8
+
+# print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
